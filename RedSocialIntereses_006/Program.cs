f7c69416c6f9847252f34c67c428873e1570930f
Kevin Alonso Acevedo Gonzalez <acevedo19972013@gmail.com>
2018-03-01 22:31:23 -0500

    Primer Commit
    Subir Inicio De Proyecto

---
+++ RedSocialIntereses_006/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocialIntereses_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
